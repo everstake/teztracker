0fa16e292875a79150a89201dc3f41c58102bfa3
MikleD <mikle0497@gmail.com>
2020-02-14 10:48:15 +0200

    Add carhagenet on info method

--- gen/restapi/embedded_spec.go
+++ gen/restapi/embedded_spec.go
@@ -1208,7 +1208,8 @@ func init() {
           {
             "enum": [
               "mainnet",
-              "babylonnet"
+              "babylonnet",
+              "carthagenet"
             ],
             "type": "string",
             "description": "Not used",
@@ -3713,7 +3714,8 @@ func init() {
           {
             "enum": [
               "mainnet",
-              "babylonnet"
+              "babylonnet",
+              "carthagenet"
             ],
             "type": "string",
             "description": "Not used",
