ce7ba643df50d3b0055bafea721bb0cf8f5d2bc7
MikleD <mikle0497@gmail.com>
2020-03-16 12:10:42 +0200

    Add indexes on accounts

--- repos/migrations/19_account_created_view.sql
+++ repos/migrations/19_account_created_view.sql
@@ -6,3 +6,11 @@ group by account_id;
 
 create index accounts_history_account_id_index
   on tezos.accounts_history (account_id);
+
+create index accounts_account_id_acc_index
+	on tezos.accounts (account_id)
+where account_id like 'tz%';
+
+create index accounts_account_id_kt_index
+	on tezos.accounts (account_id)
+where account_id like 'KT1%';
