7319c0452a594c7151f7671938af5d3fdb673e56
MikleDEverstake <md@everstake.one>
2020-10-01 15:00:11 +0300

    Fix column name

--- repos/account/account.go
+++ repos/account/account.go
@@ -66,7 +66,7 @@ func (r *Repository) List(limit, offset uint, filter models.AccountFilter) (coun
 		return 0, nil, err
 	}
 
-	db = r.db.Select("accounts.*, created_at, last_active, account_name, baker_name as delegate_name, CASE WHEN (bv.account_id IS NOT NULL) THEN TRUE ELSE FALSE	END as is_baker").
+	db = r.db.Select("accounts.*, created_at, last_active, account_name, alias as delegate_name, CASE WHEN (bv.account_id IS NOT NULL) THEN TRUE ELSE FALSE	END as is_baker").
 		Table("tezos.account_materialized_view as amv").
 		Joins("inner join tezos.accounts on accounts.account_id = amv.account_id").
 		Joins("left join tezos.known_addresses ka on accounts.delegate_value = ka.address").
@@ -100,7 +100,7 @@ func (r *Repository) Count(filter models.Account) (count int64, err error) {
 
 // Filter returns a list of accounts that sutisfies the filter.
 func (r *Repository) Filter(filter models.Account, limit, offset uint) (accounts []models.Account, err error) {
-	err = r.db.Select("accounts.*, created_at, last_active, account_name, baker_name as delegate_name").
+	err = r.db.Select("accounts.*, created_at, last_active, account_name, alias as delegate_name").
 		Model(&filter).
 		Joins("natural join tezos.account_materialized_view").
 		Joins("left join tezos.known_addresses ka on accounts.delegate_value = ka.address").
@@ -114,7 +114,7 @@ func (r *Repository) Filter(filter models.Account, limit, offset uint) (accounts
 
 // Find looks up for an account with filter.
 func (r *Repository) Find(filter models.Account) (found bool, acc models.Account, err error) {
-	if res := r.db.Select("accounts.*, created_at, last_active, account_name, baker_name as delegate_name").
+	if res := r.db.Select("accounts.*, created_at, last_active, account_name, alias as delegate_name").
 		Model(&filter).
 		Joins("natural join tezos.account_materialized_view").
 		Joins("left join tezos.known_addresses ka on accounts.delegate_value = ka.address").
