9b2fd9d0d65bc5824933128fb2fe0ef6388d6168
Messer4 <mikle0497@gmail.com>
2021-07-20 12:56:57 +0300

    New conseil deploy. Add florencenet instead of edo

--- config/main.go
+++ config/main.go
@@ -12,7 +12,7 @@ import (
 type Config struct {
 	Port                                  int
 	Mainnet                               NetworkConfig
-	Edonet                                NetworkConfig
+	Florencenet                           NetworkConfig
 	LogLevel                              string `envconfig:"LOG_LEVEL"`
 	Production                            bool
 	CounterIntervalHours                  int
--- models/network.go
+++ models/network.go
@@ -3,6 +3,6 @@ package models
 type Network string
 
 const (
-	NetworkMain Network = "main"
-	NetworkEdo  Network = "edo"
+	NetworkMain     Network = "main"
+	NetworkFlorence Network = "florence"
 )
